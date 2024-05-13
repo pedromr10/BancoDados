677b08eba8190da576818ec6c1d32904cecb35ff
daikisuga <110938759+daikisuga@users.noreply.github.com>
2024-05-13 18:14:08 -0300

    Update codigobd.sql

--- codigobd.sql
+++ codigobd.sql
@@ -1,5 +1,27 @@
 CREATE TABLE faculdade;
 
+CREATE TABLE departamento(
+  nome VARCHAR(20),
+  chefe VARCHAR(30),
+  PRIMARY KEY(nome)
+);
+
+CREATE TABLE curso(
+  nome VARCHAR(30),
+  id_curso VARCHAR(2),
+  id_deptoc VARCHAR(20),
+  PRIMARY KEY (id_curso),
+  FOREIGN KEY (id_deptoc) REFERENCES departamento(nome)
+);
+
+CREATE TABLE disciplina(
+  nome VARCHAR(30),
+  id_cursot VARCHAR(2),
+  codigo VARCHAR(10),
+  PRIMARY KEY(codigo),
+  FOREIGN KEY (id_cursot) REFERENCES curso(id_curso)
+);
+
 CREATE TABLE aluno(
   nome VARCHAR(30),
   idade INT,
@@ -23,35 +45,15 @@ CREATE TABLE professor(
 CREATE TABLE orientador(
   id_grupo VARCHAR(9),
   id_professor VARCHAR(9),
+  PRIMARY KEY(id_professor),
   FOREIGN KEY (id_grupo) REFERENCES aluno(ra),
   FOREIGN KEY (id_professor) REFERENCES professor(ra)
-);
-
-CREATE TABLE curso(
-  nome VARCHAR(30),
-  id_curso VARCHAR(2),
-  id_deptoc VARCHAR(20),
-  FOREIGN KEY (id_deptoc) REFERENCES departamento(nome)
-);
-
-CREATE TABLE departamento(
-  nome VARCHAR(20),
-  chefe VARCHAR(30),
-  PRIMARY KEY(nome)
-);
-
-CREATE TABLE disciplina(
-  nome VARCHAR(30),
-  id_cursot VARCHAR(2),
-  codigo VARCHAR(10),
-  PRIMARY KEY(codigo),
-  FOREIGN KEY (id_cursot) REFERENCES curso(id_curso)
 );
 
 CREATE TABLE tcc(
   id_grupot VARCHAR(9),
   id_orientadort VARCHAR(9),
-  FOREIGN KEY (id_orientadort) REFERENCES orientador(id_prof),
+  FOREIGN KEY (id_orientadort) REFERENCES orientador(id_professor),
   FOREIGN KEY (id_grupot) REFERENCES alnuo(ra)
 );
 
