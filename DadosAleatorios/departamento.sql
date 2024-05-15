9b6f617c30e4f8120f05091589fd0414e7d2fb1e
daikisuga <110938759+daikisuga@users.noreply.github.com>
2024-05-15 18:43:38 -0300

    Create departamento.sql

---
+++ DadosAleatorios/departamento.sql
@@ -0,0 +1,6 @@
+insert into departamento (nome, chefe) values ('Computacao', 'Geoffry');
+insert into departamento (nome, chefe) values ('Fisica', 'Gunther');
+insert into departamento (nome, chefe) values ('Marketing', 'Hertha');
+insert into departamento (nome, chefe) values ('Quimica', 'Robers');
+insert into departamento (nome, chefe) values ('Calculo', 'Arlette');
+insert into departamento (nome, chefe) values ('Engenharia', 'Walker');
