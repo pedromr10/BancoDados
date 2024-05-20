2efc9bacea4518a2820463e694c5e3a4aa8f7706
pedro <114040568+pedromr10@users.noreply.github.com>
2024-05-20 17:48:06 -0300

    Update dadosaleatoriosbd.sql

--- dadosaleatoriosbd.sql
+++ dadosaleatoriosbd.sql
@@ -1,2 +1,9 @@
-poggers poggers mt poggers
+/*Insert departamento:*/
+insert into departamento (nome, chefe) values ('Engenharia', 'Wyn');
+insert into departamento (nome, chefe) values ('Calculo', 'Teddy');
+insert into departamento (nome, chefe) values ('Marketing', 'Merralee');
+insert into departamento (nome, chefe) values ('Quimica', 'Orelie');
+insert into departamento (nome, chefe) values ('Computacao', 'Lynn');
+insert into departamento (nome, chefe) values ('Fisica', 'Erv');
 
+/*Insert aluno:*/
