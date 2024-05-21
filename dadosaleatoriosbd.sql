0da2a6c153a0eb0a61e80abff56eb253a12e2c31
daikisuga <110938759+daikisuga@users.noreply.github.com>
2024-05-20 22:29:50 -0300

    Update dadosaleatoriosbd.sql

--- dadosaleatoriosbd.sql
+++ dadosaleatoriosbd.sql
@@ -5,5 +5,23 @@ insert into departamento (nome, chefe) values (Fisica, Alan);
 insert into departamento (nome, chefe) values (Matematica, Fernando);
 
 
+/*Insert curso*/
+insert into curso (nome, id_curso, id_deptoc) values (Eletrica, CE, Engenharia);
+insert into curso (nome, id_curso, id_deptoc) values (Quimica, CQ, Engenharia);
+insert into curso (nome, id_curso, id_deptoc) values (Mecanica, CM, Engenharia);
+insert into curso (nome, id_curso, id_deptoc) values (Robotica, CR, Engenharia);
+insert into curso (nome, id_curso, id_deptoc) values (Programação, CP, Computacao);
+insert into curso (nome, id_curso, id_deptoc) values (Sistemas, CS, Computacao);
+insert into curso (nome, id_curso, id_deptoc) values (Redes, CR, Computacao);
+insert into curso (nome, id_curso, id_deptoc) values (Ciencia da Computacao, CC, Computacao);
+insert into curso (nome, id_curso, id_deptoc) values (Quantica, CQ, Fisica);
+insert into curso (nome, id_curso, id_deptoc) values (Geral, CG, Fisica);
+insert into curso (nome, id_curso, id_deptoc) values (Aplicada, CA, Fisica);
+insert into curso (nome, id_curso, id_deptoc) values (Calculos, CC, Matematica);
+insert into curso (nome, id_curso, id_deptoc) values (Estatistica, CE, Matematica);
+insert into curso (nome, id_curso, id_deptoc) values (Probabilidade, CP, Matematica);
+insert into curso (nome, id_curso, id_deptoc) values (Algebra, CA, Matematica);
+insert into curso (nome, id_curso, id_deptoc) values (Geometria, CG, Matematica);
+insert into curso (nome, id_curso, id_deptoc) values (Linear, CL, Matematica);
 /*Insert aluno:*/
 
