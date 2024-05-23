d74edc03ba1e095fd4e1be03327318e084c0940b
pedro <114040568+pedromr10@users.noreply.github.com>
2024-05-23 18:30:08 -0300

    Update queriesbd.sql

--- queriesbd.sql
+++ queriesbd.sql
@@ -1,1 +1,13 @@
-a
+/*Querie 1:*/
+SELECT aluno.nome AS nome_aluno, 
+       disciplina.codigo AS codigo_disciplina, 
+       disciplina.nome AS nome_disciplina, 
+       historico.semestre, 
+       historico.ano, 
+       historico.nota
+FROM aluno
+JOIN historico ON aluno.ra = historico.id_alunoh
+JOIN disciplina ON historico.id_disciplinah = disciplina.codigo
+WHERE aluno.nome = 'Gustavo';
+
+/*Querie 2:*/
