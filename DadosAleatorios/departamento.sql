126f00d4b06eab479f1bf6ab8d90579a1ba32a4a
daikisuga <110938759+daikisuga@users.noreply.github.com>
2024-05-23 18:26:13 -0300

    Delete DadosAleatorios directory

--- DadosAleatorios/departamento.sql
+++
@@ -1,6 +0,0 @@
-insert into departamento (nome, chefe) values ('Computacao', 'Geoffry');
-insert into departamento (nome, chefe) values ('Fisica', 'Gunther');
-insert into departamento (nome, chefe) values ('Marketing', 'Hertha');
-insert into departamento (nome, chefe) values ('Quimica', 'Robers');
-insert into departamento (nome, chefe) values ('Calculo', 'Arlette');
-insert into departamento (nome, chefe) values ('Engenharia', 'Walker');
