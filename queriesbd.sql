a387f3aec4a4f6eb304f77c363dfb5034921d0e3
pedro <114040568+pedromr10@users.noreply.github.com>
2024-05-23 18:58:48 -0300

    Update queriesbd.sql

--- queriesbd.sql
+++ queriesbd.sql
@@ -1,13 +1,27 @@
 /*Querie 1:*/
-SELECT aluno.nome AS nome_aluno, 
-       disciplina.codigo AS codigo_disciplina, 
-       disciplina.nome AS nome_disciplina, 
-       historico.semestre, 
-       historico.ano, 
-       historico.nota
+SELECT aluno.nome, disciplina.codigo, disciplina.nome, historico.semestre, historico.ano, historico.nota
 FROM aluno
-JOIN historico ON aluno.ra = historico.id_alunoh
-JOIN disciplina ON historico.id_disciplinah = disciplina.codigo
+INNER JOIN historico ON aluno.ra = historico.id_alunoh
+INNER JOIN disciplina ON historico.id_disciplinah = disciplina.codigo
 WHERE aluno.nome = 'Gustavo';
 
 /*Querie 2:*/
+SELECT historicoprof.id_professorhp,d.codigo,d.nome,historicoprof.semestre, historicoprof.ano
+FROM historicoprof
+JOIN disciplina d ON historicoprof.id_disciplinahp = d.codigo
+WHERE historicoprof.id_professorhp = '185906565';
+
+/*Querie 3:*/
+
+/*Querie 4:*/
+SELECT professor.nome,d.nome
+FROM professor
+INNER JOIN departamento d ON professor.nome = d.chefe;
+
+/*Querie 5:*/
+SELECT aluno.nome, aluno.ra, t.tcc_grupo, p.nome
+FROM aluno 
+INNER JOIN tcc t ON aluno.tcc_a = t.tcc_grupo
+INNER JOIN orientador o ON t.tcc_grupo = o.id_grupoo
+INNER JOIN professor p ON o.id_professor = p.ra
+WHERE t.tcc_grupo = '131201912';
